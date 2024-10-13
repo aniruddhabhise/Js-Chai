79fac7116fc94e2196779ad3a3e32b71d2236663
aniruddhabhise <118464912+aniruddhabhise@users.noreply.github.com>
2024-10-13 13:33:32 +0000

    comparison

---
+++ 01_Basics/03_comparison.js
@@ -0,0 +1,21 @@
+const score="33"
+
+console.log(typeof score);
+
+let valueInNumber=Number(score)
+
+console.log(typeof valueInNumber);
+
+let userLoggedIn=1
+
+let booleanUserLogedIn=Boolean(userLoggedIn)
+
+console.log(booleanUserLogedIn);
+
+
+// "33" => 33
+// "33abc" => NaN (Not A Number)
+// True => 1;  False=> 0
+
+// "" => False
+// "Ani" => True
