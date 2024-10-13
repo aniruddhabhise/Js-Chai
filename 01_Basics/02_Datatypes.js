0e5b06205de05b7b99024ef5b6ef6615b206f133
aniruddhabhise <118464912+aniruddhabhise@users.noreply.github.com>
2024-10-13 09:33:11 +0000

    datatypes

---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treats  all js code as per newer version.
+
+// alert(3 + 3) we are using nodejs not browser
+
+console.log(3+3);
+
+console.log('Aniruddha')
+
+// Number
+// bigint
+// strinf => ""
+// bolean => true/false
+// null
+ // undefined ( value not assigned )
+ // object
+
+ console.log(typeof undefined); // undefined
+ console.log(typeof null);  // object type
+
+
+ 
