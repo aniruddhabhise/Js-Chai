4e1eb1b70db669bea6a1085e864e37fbde205dfd
aniruddhabhise <118464912+aniruddhabhise@users.noreply.github.com>
2024-10-13 19:04:56 +0530

    new
    ss

---
+++ 01_Basics/04_ConversionOperations.js
@@ -0,0 +1,28 @@
+let value=3
+let negativeValue=-value
+
+console.log(negativeValue);
+
+
+// console.log(1+"2"); //12
+
+// console.log("1"+2) // 12
+
+// console.log(1+2+"2"); // 32
+
+// console.log("1"+2+2); // 122
+
+// Postfix increment a++
+let a=9
+b=a++
+// console.log(a);
+// console.log(b);
+
+// Prefix increment ++a
+let m=9
+n=++m
+console.log(m);
+console.log(n);
+ // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+
+//  >?
---
+++ 01_Basics/05_Comparison.js
@@ -0,0 +1,23 @@
+// console.log(2>1);
+// console.log(2<1);
+// console.log(2<=1);
+// console.log(2>=1);
+// console.log(2==1);
+// console.log(2!=1);
+
+// console.log("2"<1);  // coverts string to a no.
+// console.log("02">1); //same 
+
+// console.log(null>0); // Null is converted to 0 so False o/p
+// console.log(null==0); 
+// console.log(null>=0);  // Null is converted to 0 so True o/p
+
+// console.log(undefined==0);  //can not predict conversion of undefined
+// console.log(undefined>0);
+// console.log(undefined<0);
+
+// strict check
+// ===
+// checks data type and value both without conversion
+// ?
+console.log("2"===2);
