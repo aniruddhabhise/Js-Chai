b60012fdcec3cbff209c2726240648117ba5b393
aniruddhabhise <118464912+aniruddhabhise@users.noreply.github.com>
2024-10-13 08:33:26 +0000

    changes

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId=144535
+
+let AccoutName='Ani'
+let accountPassword='123'
+accountCity='Mumbai' //can be declared this way also but not a good practice
+
+/* var is not used in the js anymore 
+due to block isse */
+console.log(AccoutName);
+console.table([accountId,accountPassword,AccoutName,accountCity])
